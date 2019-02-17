fc1b69f5ede6daf88e5737a8074a0ac6ffa1cb1d
Ioannis Batsios <ioannisbatsios@gmail.com>
2019-02-17 02:30:18 -0500

    created the login through gmail api page

--- login.php
+++ login.php
@@ -1,16 +1,21 @@
-<?php session_start(); ?>
-<?php include('header.php'); ?>
+<?php session_start(); 
+include('header.php');
+require_once "checklogin.php";
+$loginURL = $client->createAuthUrl();
 
+?>
 <body>
-
-<div class="center-form">
-	<form action="checklogin.php" method="post">
-		<label>Email:</label>
-			<input type="text" name="login_email"><br>
-		<label>Password:</label>
-			<input type="text" name="password">
-		<div class="buttonHolder">
-			<input type="submit" name="Submit" class="button" value="Submit">
-	</form>
+<div class="container" style="margin-top: 100px">
+	<div class="row justify-content-center">
+			<div class="col-md-6 col-offset-3" align="center">
+				<form action="checklogin.php" method="post">
+					<input placeholder="Email" name="Email" class="form-control"><br />
+					<input placeholder="Password" type="password" name="password"  class="form-control"><br />
+					<input type="submit" value="Log In" class="btn btn-primary">
+					<input type="button" onclick="window.location = '<?php echo $loginURL ?>';" value="Log In With Google" class="btn btn-danger">
+				</form>
+			</div>
+		</div>
+	</div>
 
 <?php include('footer.php'); ?>
