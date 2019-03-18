9c4bd4394afa4ee5766aa5095cdd7ac64065fe21
Ioannis Batsios <ioannisbatsios@gmail.com>
2019-03-18 08:33:28 -0400

    made some changes. trying to get the script to work for user_type, but unsuccessful.

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -6,21 +6,19 @@
     </div>
     <div class="col-lg-3"></div>
 </div>
-<div class="container">
     <form action="" method="post" style="padding-top: 25px;">
         <div class="row">
-            <div class="col-lg-3"></div>
-            <div class="col-sm">
+            <div class="col-lg-2"></div>
+            <div class="col-lg">
                 <input type="text" class="form-control" id="fname" placeholder="First Name">
                 <br>
                 <input type="text" class="form-control" id="lname" placeholder="Last Name">
                 <br>
                 <input type="text" class="form-control" id="address" placeholder="Street Address">
                 <br>
+                <input type="text" class="form-control" id="city" placeholder="City">
+                <br>
                 <div class="row">
-                    <div class="col-sm">
-                        <input type="text" class="form-control" id="city" placeholder="City">
-                    </div>
                     <div class="col-sm">
                         <!-- TODO: define states somewhere and make the state field a drop-down menu -->
                         <input type="text" class="form-control" id="state" placeholder="State">
@@ -33,7 +31,7 @@
                 <!-- Phone number is not automatically validated by the form type. -->
                 <input type="tel" class="form-control" id="phone" placeholder="Phone Number">
             </div>
-            <div class="col-sm">
+            <div class="col-lg">
                 <input type="email" class="form-control" id="email" placeholder="Email">
                 <br>
                 <input type="email" class="form-control" id="confirm_email" placeholder="Retype Email">
@@ -42,15 +40,14 @@
                 <br>
                 <input type="text" class="form-control" id="confirm_password" placeholder="Verify Password">
             </div>
-            <div class="col-lg-3"></div>
+            <div class="col-lg-2"></div>
         </div>
-        <div class="row" style="padding-top: 15px;">
+        <div class="row" style="padding-top: 35px;">
             <div class="col-lg-4"></div>
             <input type="submit" value="Register" class="col-lg-4 btn btn-primary">
             <div class="col-lg-4"></div>
         </div>
     </form>
-</div>
 <div class="row" style="padding-top: 25px;">
     <div class="col-lg-4"></div>
     <div class="col-lg-4"><h5>Already have an account?</h5></div>
