7aee0329b603842ae9a36944b3956e7c950aee86
Ioannis <ioannisbatsios@gmail.com>
2019-04-10 19:33:01 -0400

    added navbar

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,24 +4,26 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<title><?php echo SITE_NAME ?></title>
+        <meta name="google-signin-client_id" content="{{client_id}}">
+		<title><?php echo SITENAME ?></title>
 
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" href="<?php echo URL_ROOT; ?>/css/style.css">
-
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="https://apis.google.com/js/api/js"></script>
 		<![endif]-->
     <body>
+    <?php require APP_ROOT . '/views/inc/navbar.php'; ?>
         <div class="container">
             <div class="wrap">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1><a href="<?php echo URL_ROOT; ?>"><?php echo SITE_NAME; ?></a></h1>
+                        <h1><?php echo SITENAME ?></h1>
                     </div>
                 </div>
 	
