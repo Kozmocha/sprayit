5b0443880a84a288b028f897aac6149ec4957b23
Ioannis Batsios <ioannisbatsios@gmail.com>
2019-04-17 11:01:32 -0400

    Fixed merge conflicts

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,10 +2,6 @@
 
 require_once APP_ROOT . '/models/User.php';
 
-<<<<<<< HEAD
-    public function __construct() {
-        $this->userModel = $this->model('User');
-=======
 /**
  * Users controller: This controller is used to handle all data transfers for any view within the 'users' directory.
  * Each method corresponds to a view (names must be EXACTLY the same).
@@ -20,7 +16,6 @@ class Users extends Controller {
      */
     public function index() {
         Redirect::to('users/login');
->>>>>>> 7aee0329b603842ae9a36944b3956e7c950aee86
     }
 
     /**
@@ -31,171 +26,7 @@ class Users extends Controller {
         if (Session::isLoggedIn()) {
             Redirect::to('pages/calendar');
         }
-<<<<<<< HEAD
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $post = Session::sanitizePost();
-            $data = [
-                'email' => trim($post['email']),
-                'password' => trim($post['password'])
-            ];
-            // TODO: For future error identification.
-            $errors = [
-                'email_err' => '',
-                'password_err' => ''
-            ];
-            $isLoggedIn = User::authenticate($post['email'], $post['password']);
-            if ($isLoggedIn) {
-                Redirect::to('pages/calendar');
-            } else {
-                $this->view('users/login', $data, $errors);
-            }
-        } else {
-            $data = [
-                'email' => '',
-                'password' => ''
-            ];
-            $this->view('users/login', $data);
-        }
-    }
 
-    public function createUserSession($user){
-        $_SESSION['user_id'] = $user->id;
-        $_SESSION['user_email'] = $user->email;
-//        $_SESSION['user_name'] = $user->name;
-
-        redirect('calendar');
-    }
-
-    public function client_register() {
-
-        $this->view('users/client_register');
-
-        //Check for POST
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            //Process the form
-
-            //Sanitize POST data
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-            //Init Data
-            $_data =[
-                'fname' => trim($_POST['fname']),
-                'lname' => trim($_POST['lname']),
-                'city' => trim($_POST['city']),
-                'state' => trim($_POST['state']),
-                'zip' => trim($_POST['zip']),
-                'phone' => trim($_POST['phone']),
-                'email' => trim($_POST['email']),
-                'confirm_email' => trim($_POST['confirm_email']),
-                'password' => trim($_POST['password']),
-                'confirm_password' => trim($_POST['confirm_password']),
-                'fname_err' => '',
-                'lname_err' => '',
-                'city_err' => '',
-                'state_err' => '',
-                'zip_err' => '',
-                'phone_err' => '',
-                'email_err' => '',
-                'confirm_email_err' => '',
-                'password_err' => '',
-                'confirm_password_err' => '',
-            ];
-
-            //Validate First Name
-            if(empty($_data['fname'])){
-                $_data['fname_err'] = 'Please enter a first name';
-            }
-
-            //Validate Last Name
-            if(empty($_data['lname'])){
-                $_data['lname_err'] = 'Please enter a last name';
-            }
-
-            //Validate city
-            if(empty($_data['city'])){
-                $_data['city_err'] = 'Please enter a city';
-            }
-
-            //Validate Email
-            if(empty($_data['state'])){
-                $_data['state_err'] = 'Please enter a state';
-            }
-
-            //Validate Email
-            if(empty($_data['zip'])){
-                $_data['zip_err'] = 'Please enter a zipcode';
-            }
-
-            //Validate Phone
-            if(empty($_data['phone'])){
-                $_data['phone_err'] = 'Please enter a phone number';
-            }
-
-            //Validate Email
-            if(empty($_data['email'])){
-                $_data['email_err'] = 'Please enter email';
-            }
-
-            //Validate Ccnfirm_email
-            if(empty($_data['confirm_email'])){
-                $_data['confirm_email_err'] = 'Please enter email';
-            }
-
-            //Validate Password
-            if(empty($_data['password'])){
-                $_data['password_err'] = 'Please enter a password';
-            } elseif (strlen($_data['password']) < 6){
-                $_data['password_err'] = 'Password must be at least 6 characters';
-            }
-
-            //Validate Confirm Password
-            if(empty($_data['confirm_password'])){
-                $_data['confirm_password_err'] = 'Please confirm password';
-            } else {
-                if($_data['password'] != $_data['confirm_password_err']){
-                    $_data['confirm_password_err'] = 'Passwords do not match';
-                }
-            }
-
-            //Make sure errors are empty
-            if(empty($_data['fname_err']) && empty($_data['lname_err'])&& empty($_data['city_err'])&& empty($_data['state_err'])
-                && empty($_data['zip_err'])&& empty($_data['phone_err'])&& empty($_data['email_err'])&& empty($_data['confirm_email_err'])
-                && empty($_data['password'])&& empty($_data['confirm_password_err'])){
-                //Validate
-                die('SUCCESS');
-            } else {
-                //Load view with errors
-                $this->view('Users/client_register', $_data);
-            }
-
-        } else {
-            //Init data
-            $_data =[
-                'fname' => '',
-                'lname' => '',
-                'city' => '',
-                'state' => '',
-                'zip' => '',
-                'phone' => '',
-                'email' => '',
-                'confirm_email' => '',
-                'password' => '',
-                'confirm_password' => '',
-                'fname_err' => '',
-                'lname_err' => '',
-                'city_err' => '',
-                'state_err' => '',
-                'zip_err' => '',
-                'phone_err' => '',
-                'email_err' => '',
-                'confirm_email_err' => '',
-                'password_err' => '',
-                'confirm_password_err' => '',
-            ];
-
-            //Load view
-//            $this->view('users/client_register', $data);
-=======
 
         if (Session::isPost()) {
             $post = Session::sanitizePost();
@@ -210,39 +41,9 @@ class Users extends Controller {
             }
         } else {
             $this->view('users/login');
->>>>>>> 7aee0329b603842ae9a36944b3956e7c950aee86
         }
     }
 
-    /**
-     * Contractor registration: Transfers registration form data (contractor-specific) from the contractor_register
-     * view to the User model to be handled. NOT YET IMPLEMENTED.
-     */
-    public function contractor_register() {
-<<<<<<< HEAD
-        $data = [
-            'title' => SITE_NAME,
-            'description' => MOTTO
-        ];
-
-        $this->view('users/contractor_register', $data);
-
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            //Sanitize POST data
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-            //Init Data
-            $_data = [
-                'coname' => trim($_POST['coname'])
-            ];
-
-            //Validate First Name
-            if(empty($_data['coname'])){
-                $_data['coname_err'] = 'Please enter a first name';
-            }
-
-        }
-    }
 
     public function google_login(){
 
@@ -280,47 +81,6 @@ class Users extends Controller {
 
         }
 
-=======
         $this->view('users/contractor_register');
     }
-
-    /**
-     * Client registration: Transfers registration form data (client-specific) from the client_register view to the
-     * User model to be handled. NOT YET IMPLEMENTED.
-     */
-    public function client_register() {
-        $this->view('users/client_register');
->>>>>>> 7aee0329b603842ae9a36944b3956e7c950aee86
-    }
-
-    /**
-     * User type: Gets the data from the user_type view and asks the User model which type of user they are. The
-     * response determines which redirect to use or if the view is to be loaded again.
-     */
-    public function user_type() {
-<<<<<<< HEAD
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if ($_POST["user_type"] == "client") {
-                Redirect::to('users/client_register');
-                //exit;
-            }
-            if ($_POST["user_type"] == "contractor") {
-                Redirect::to('users/contractor_register');
-                //exit;
-            }
-        }
-
-=======
-        if (Session::isPost()) {
-            if (User::isClient()) {
-                Redirect::to('users/client_register');
-            } else if (User::isContractor()) {
-                Redirect::to('users/contractor_register');
-            }
-        }
->>>>>>> 7aee0329b603842ae9a36944b3956e7c950aee86
-        $this->view('users/user_type');
-    }
-
-
 }
--- app/libraries/Redirect.php
+++ app/libraries/Redirect.php
@@ -1,15 +1,13 @@
 <?php
 
-class Redirect {
-    public static function to($_page = null) {
-        if($_page) {
+class Redirect
+{
+    public static function to($_page = null)
+    {
+        if ($_page) {
             header('location: ' . URL_ROOT . '/' . $_page);
         } else {
             header('location: ' . URL_ROOT . '/pages/not_found');
         }
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 7aee0329b603842ae9a36944b3956e7c950aee86
 }
