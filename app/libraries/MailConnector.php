2398ca725c43851df1bf22a562f5629b2827bbcd
Christopher <csteven015@gmail.com>
2019-04-24 11:04:31 -0400

    Updated documentation for emailing code.

--- app/libraries/MailConnector.php
+++ app/libraries/MailConnector.php
@@ -7,6 +7,11 @@ class MailConnector {
 
     private static $translator = MAIL_TRANSLATOR;
 
+    /**
+     * MailConnector send method: Bridge method to the email translator class's send method.
+     *
+     * @author Christopher Thacker
+     */
     public static function send($_to = '', $_toName = '', $_subject = '', $_text = '', $_html = '') {
         $mailTranslator = static::$translator;
         $mailTranslator::send($_to, $_toName, $_subject, $_text, $_html);
