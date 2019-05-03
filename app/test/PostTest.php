80a5ca6425720d340b66d650c1316b9a3698422a
Christopher <csteven015@gmail.com>
2019-05-02 20:59:23 -0400

    Added test cases.

--- app/test/PostTest.php
+++ app/test/PostTest.php
@@ -1,9 +1,21 @@
 <?php
 
-require_once APP_ROOT . '/models/Post.php';
+// Must manually add these requires, as the command line does not automatically load everything through SprayItCore.
+require_once '../models/Post.php';
+require_once '../libraries/Auth.php';
 
 class PostTest {
 
+    /**
+     * Method used to test the checkFields() method in the Post model.
+     *
+     * @param $_title
+     * @param $_body
+     * @param $_expected
+     * @return bool
+     *
+     * @author Christopher Thacker
+     */
     public static function testCheckFields($_title, $_body, $_expected) {
         $result = Post::checkFields($_title, $_body);
 
@@ -14,35 +26,40 @@ class PostTest {
         }
     }
 
+    /**
+     * Method used to begin tests on the selected method(s).
+     *
+     * @author Christopher Thacker
+     */
     public static function systemTest() {
         // Normal test cases below.
-        echo ("Testing normal case 01.");
-        echo ((self::testCheckFields("Title 1", "This is the body!", true)) ? "Pass" : "Fail");
+        echo ("Testing normal case 01.\n");
+        echo ((self::testCheckFields("Title 1", "This is the body!", true)) ? "Pass\n\n" : "Fail\n\n");
 
-        echo ("Testing normal case 02.");
-        echo ((self::testCheckFields("Título 2", "¡Este es el cuerpo!", true)) ? "Pass" : "Fail");
+        echo ("Testing normal case 02.\n");
+        echo ((self::testCheckFields("Título 2", "¡Este es el cuerpo!", true)) ? "Pass\n\n" : "Fail\n\n");
 
-        echo ("Testing normal case 03.");
-        echo ((self::testCheckFields("タイトル 3", "これが体です！", true)) ? "Pass" : "Fail");
+        echo ("Testing normal case 03.\n");
+        echo ((self::testCheckFields("タイトル 3", "これが体です！", true)) ? "Pass\n\n" : "Fail\n\n");
 
         // Edge test cases below.
-        echo ("Testing edge case 01.");
-        echo ((self::testCheckFields("", "This is the body!", false)) ? "Pass" : "Fail");
+        echo ("Testing edge case 01.\n");
+        echo ((self::testCheckFields("", "This is the body!", false)) ? "Pass\n\n" : "Fail\n\n");
 
-        echo ("Testing edge case 02.");
-        echo ((self::testCheckFields("Title 2", "", false)) ? "Pass" : "Fail");
+        echo ("Testing edge case 02.\n");
+        echo ((self::testCheckFields("Title 2", "", false)) ? "Pass\n\n" : "Fail\n\n");
 
-        echo ("Testing edge case 03.");
-        echo ((self::testCheckFields("", "", false)) ? "Pass" : "Fail");
+        echo ("Testing edge case 03.\n");
+        echo ((self::testCheckFields("", "", false)) ? "Pass\n\n" : "Fail\n\n");
 
         // Error test cases below.
-        echo ("Testing error case 01.");
-        echo ((self::testCheckFields("Title 1", null, false)) ? "Pass" : "Fail");
+        echo ("Testing error case 01.\n");
+        echo ((self::testCheckFields("Title 1", null, false)) ? "Pass\n\n" : "Fail\n\n");
 
-        echo ("Testing error case 02.");
-        echo ((self::testCheckFields(null, "Body stuff here", false)) ? "Pass" : "Fail");
+        echo ("Testing error case 02.\n");
+        echo ((self::testCheckFields(null, "Body stuff here", false)) ? "Pass\n\n" : "Fail\n\n");
 
-        echo ("Testing error case 03.");
-        echo ((self::testCheckFields("TRUNCATE TABLE `posts`", "TRUNCATE TABLE `posts`", true)) ? "Pass" : "Fail");
+        echo ("Testing error case 03.\n");
+        echo ((self::testCheckFields("<script>alert('YOU BEEN HACKED BOI')</script>", "TRUNCATE TABLE `posts`", true)) ? "Pass\n\n" : "Fail\n\n");
     }
 }
