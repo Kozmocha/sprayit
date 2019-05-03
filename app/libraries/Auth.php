21a2c371418f96bc6aee40f206e4f27a1ec4c4c8
Christopher <csteven015@gmail.com>
2019-05-03 10:35:04 -0400

    Added backend check for post author.

--- app/libraries/Auth.php
+++ app/libraries/Auth.php
@@ -28,4 +28,17 @@ class Auth {
     public static function sanitizeEmail($_email) {
         return filter_var($_email, FILTER_SANITIZE_EMAIL);
     }
+
+    /**
+     * Checks to make sure two values are equal to each other.
+     *
+     * @param $_value1
+     * @param $_value2
+     * @return bool
+     *
+     * @author Christopher Thacker
+     */
+    public static function isEqual($_value1, $_value2) {
+        return $_value1 == $_value2;
+    }
 }
